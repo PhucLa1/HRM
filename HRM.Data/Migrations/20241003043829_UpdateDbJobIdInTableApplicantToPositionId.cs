ce332899c59f524eea9e7632fe16d776c04bd316
PhucLa1 <phucminhbeos@gmail.com>
2024-10-05 08:18:41 +0700

    change_jobId_in_applicant_table_to_position_id

---
+++ HRM.Data/Migrations/20241003043829_UpdateDbJobIdInTableApplicantToPositionId.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDbJobIdInTableApplicantToPositionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Applicants_Positions_PositionId",
+                table: "Applicants");
+
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                table: "Applicants");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PositionId",
+                table: "Applicants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Applicants_Positions_PositionId",
+                table: "Applicants",
+                column: "PositionId",
+                principalTable: "Positions",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Applicants_Positions_PositionId",
+                table: "Applicants");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PositionId",
+                table: "Applicants",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobId",
+                table: "Applicants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Applicants_Positions_PositionId",
+                table: "Applicants",
+                column: "PositionId",
+                principalTable: "Positions",
+                principalColumn: "id");
+        }
+    }
+}
