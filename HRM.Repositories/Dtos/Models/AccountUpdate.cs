e6dd36cac9c3f18b1d4342f2942f9989edaaeb4f
PhucLa1 <phucminhbeos@gmail.com>
2024-11-11 11:16:32 +0700

    profile current

---
+++ HRM.Repositories/Dtos/Models/AccountUpdate.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace HRM.Repositories.Dtos.Models
+{
+    public class AccountUpdate
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+    }
+
+
+    public class AccountUpdateValidator : AbstractValidator<AccountUpdate>
+    {
+        public AccountUpdateValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .WithMessage("User name không được bỏ trống");
+            RuleFor(x => x.Password)
+                   .NotEmpty()
+                   .WithMessage("Mật khẩu không được để trống");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email không được bỏ trống")
+                .EmailAddress()
+                .WithMessage("Email không đúng định dạng");
+        }
+    }
+}
---
+++ HRM/Swagger/Examples/Responses/LabelDescriptionsResponseExample.cs
@@ -0,0 +1,39 @@
+﻿using HRM.Repositories.Dtos.Results;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace HRM.Apis.Swagger.Examples.Responses
+{
+    public class LabelDescriptionsResponseExample : IExamplesProvider<ApiResponse<List<LabelDescriptions>>>
+    {
+        public ApiResponse<List<LabelDescriptions>> GetExamples()
+        {
+            return new ApiResponse<List<LabelDescriptions>>
+            {
+                IsSuccess = true,
+                Metadata = new List<LabelDescriptions>
+                {
+                    new LabelDescriptions
+                    {
+                        Name = "Label A",
+                        Id = 1,
+                        Descriptions = new List<string>
+                        {
+                            "{'0' : '0,111', ..}",
+                            "{'0' : '0,111', ..}"
+                        }
+                    },
+                    new LabelDescriptions
+                    {
+                        Name = "Label B",
+                        Id = 2,
+                        Descriptions = new List<string>
+                        {
+                            "{'0' : '0,111', ..}",
+                            "{'0' : '0,111', ..}"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
